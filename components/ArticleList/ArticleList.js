6ec06f600a0dd9cb15a62e7f95a165411afa0926
CatsAreMLG <joshuadgon@gmail.com>
2021-02-08 13:43:49 -0600

    Add null conditional

--- components/ArticleList/ArticleList.js
+++ components/ArticleList/ArticleList.js
@@ -2,13 +2,14 @@ import Article from './Article/Article'
 import articleStyles from '../../styles/Article.module.css'
 
 const ArticleList = ({articles}) => {
-    return (
+    if (articles) return (
       <div className={articleStyles.grid}>
         {articles.map((article) => (
           <Article key={article.slug} article={article} />
         ))}
       </div>
-    );
+    )
+    else return <div>Loading...</div>
 }
  
 export default ArticleList;
